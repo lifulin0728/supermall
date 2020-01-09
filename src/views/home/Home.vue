2ab4526132f706f7028bd59f7f62c06bc136bdaf
liulin <1055856889@qq.com>
2020-01-09 19:53:56 +0800

    @1.1.3

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,11 +8,28 @@
 
 <script>
 	import NavBar from 'components/common/navbar/NavBar'
+	import {getHomeMultidata} from 'network/home'
 	
 	export default {
 		name: "Home",
 		components: {
 			NavBar
+		},
+		data() {
+			return {
+				// result: null
+				banners: [],
+				recommends: []
+			}
+		},
+		created() {
+			// 1.请求多个数据
+			getHomeMultidata().then(res => {
+				console.log(res);
+				// this.result = res;
+				this.banners = res.data.banner.list;
+				this.recommends = res.data.recommend.list;
+			})
 		}
 	}
 </script>
