db23a1dc2a3c89d760427f324de3702c1a3a95e5
liulin <1055856889@qq.com>
2020-01-21 11:49:49 +0800

    @1.2.3

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,8 +1,12 @@
 <template>
 	<div id="detail">
-		<detail-nav-bar/>
-		<detail-swiper :top-images="topImages"/>
-		<detail-base-info :goods="goods"/>
+		<detail-nav-bar class="detail-nav"/>
+		<scroll class="content" ref="scroll">
+			<detail-swiper :top-images="topImages"/>
+			<detail-base-info :goods="goods"/>
+			<detail-shop-info :shop="shop"/>
+			<detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
+		</scroll>
 	</div>
 </template>
 
@@ -10,21 +14,30 @@
 	import DetailNavBar from './childComps/DetailNavBar'
 	import DetailSwiper from './childComps/DetailSwiper'
 	import DetailBaseInfo from './childComps/DetailBaseInfo'
+	import DetailShopInfo from './childComps/DetailShopInfo'
+	import DetailGoodsInfo from './childComps/DetailGoodsInfo'
 	
-	import {getDetail, Goods} from 'network/detail'
+	import Scroll from 'components/common/scroll/Scroll'
+	
+	import {getDetail, Goods, Shop} from 'network/detail'
 	
 	export default {
 		name: "Detail",
 		components: {
 			DetailNavBar,
 			DetailSwiper,
-			DetailBaseInfo
+			DetailBaseInfo,
+			DetailShopInfo,
+			DetailGoodsInfo,
+			Scroll
 		},
 		data() {
 			return {
 				iid: null,
 				topImages: [],
-				goods: {}
+				goods: {},
+				shop: {},
+				detailInfo: {}
 			}
 		},
 		created() {
@@ -40,11 +53,38 @@
 				
 				// 2.获取商品信息
 				this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services);
+				
+				// 3.获取店铺信息
+				this.shop = new Shop(data.shopInfo)
+				
+				// 4.保存商品的详细数据
+				this.detailInfo = data.detailInfo
 			})
+		},
+		methods: {
+			imageLoad() {
+				this.$refs.scroll.refresh();
+			}
 		}
 	}
 </script>
 
 <style scoped>
+	#detail {
+		position: relative;
+		z-index: 9;
+		background-color: #fff;
+		height: 100vh;
+		overflow: hidden;
+	}
 	
+	.detail-nav {
+		position: relative;
+		z-index: 9;
+		background-color: #fff;
+	}
+	
+	.content {
+		height: calc(100% - 44px);
+	}
 </style>
